8750ca9f62e45786045beccad57c6a56dfdcf6e6
scue <linkscue@gmail.com>
2013-03-15 15:17:18 +0800

    first commit

---
+++ smalimeld.sh
@@ -0,0 +1,29 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: smalimeld.sh
+# 
+#         USAGE: ./smalimeld.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: linkscue (scue), linkscue@gmail.com
+#       CREATED: 2013年03月13日 22时48分05秒 HKT
+#     COPYRIGHT: Copyright (c) 2013, linkscue
+#      REVISION: 0.1
+#  ORGANIZATION: ATX破晓组
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+if [[ $# == 0 ]]; then
+    echo "$(basename $0) <smali_file>"
+    exit 1
+fi
+smali="$1"
+export LANGUAGE=zh_CN.UTF-8
+meld temp/old_smali/"$smali"  temp/new_smali/"$smali" noline/"$smali" &
+
